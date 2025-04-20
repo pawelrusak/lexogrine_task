b885f72f9fe3150b4ac109eee3c65947f845d5c0
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2025-04-20 22:06:45 +0200

    feat: add redirect route to pricing page if user is logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router";
 
+import AuthRoute from "@/utils/AuthRoute";
 import AuthPage from "@/features/auth/AuthPage";
 import PricingPage from "@/features/pricing/PricingPage";
 
 const App = () => {
   return (
     <Routes>
-      <Route index element={<AuthPage />} />
+      <Route element={<AuthRoute redirectPath="/pricing" />}>
+        <Route index element={<AuthPage />} />
+      </Route>
       <Route path="/pricing" element={<PricingPage />} />
     </Routes>
   );
